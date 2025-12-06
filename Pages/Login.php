3c8e629d5752ed9e68586b2ea2ac6b9cfb2b20ff
ifti-77 <23-51177-1@student.aiub.edu>
2025-12-06 14:19:23 +0600

    Created Login page

---
+++ Pages/Login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="Designs/login.css">
+</head>
+
+<body>
+    <div class="formContainer">
+        <h1>Salah Tracker</h1>
+        <h3>Login</h3>
+        <form action="" method="post">
+            <input type="text" name="email" placeholder="Email">
+            <input type="password" name="password" placeholder="Password">
+            <div>
+                <label><input type="checkbox">Remember me</label>
+                <label><a href="#">Forgot pasword?</a></label>
+            </div>
+            <input type="submit" value="Login">
+            <label>Don't have an account? <a href="#">Create account</a></label>
+        </form>
+    </div>
+</body>
+
+</html>
